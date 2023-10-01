2b4ef06be03d66d9a3eedf2b7899700748fdfbc5
Kevin Breit <kevin.breit@kevinbreit.net>
2023-09-30 22:11:55 -0500

    Add LazyGit plugin

---
+++ nvim/lua/kbreit/plugins/lazygit.lua
@@ -0,0 +1,13 @@
+return {
+  "kdheepak/lazygit.nvim",
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Toggle LazyGit" })
+  end,
+}
