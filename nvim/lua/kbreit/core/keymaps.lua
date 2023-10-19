c9f1cac0c2d232b891735bbc2043ce4e04d6e1e5
Kevin Breit <kevin.breit@kevinbreit.net>
2023-10-19 08:29:37 -0500

    Add splits with the same file

--- nvim/lua/kbreit/core/keymaps.lua
+++ nvim/lua/kbreit/core/keymaps.lua
@@ -16,12 +16,13 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
-
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sdv", "<cmd>vsp<CR>", { desc = "Split window vertically with the same file" })
+keymap.set("n", "<leader>sdh", "<cmd>sp<CR>", { desc = "Split window horizontally with the same file" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
