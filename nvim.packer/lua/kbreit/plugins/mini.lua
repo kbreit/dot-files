a3c26808cef14090f85b39b615a473f8b8224e78
Kevin Breit <kevin.breit@kevinbreit.net>
2023-12-09 15:03:18 -0600

    Change to Catppuccin and remove nvim.packer

--- nvim.packer/lua/kbreit/plugins/mini.lua
+++
@@ -1,5 +0,0 @@
-local status, mini = pcall(require, "mini")
-if not status then
-  return
-end
-
--- nvim/lua/kbreit/plugins/colorscheme.lua
+++ nvim/lua/kbreit/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-  "bluz71/vim-nightfly-guicolors",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
     -- load the colorscheme here
-    vim.cmd([[colorscheme nightfly]])
+    vim.cmd([[colorscheme catppuccin]])
   end,
 }
